eaddea792fc3cb8a0e9e4023fb0c565714d05fcf
Antti Jaaniste <antti.jaaniste@mg.ee>
2017-08-04 10:36:31 +0300

    First commit

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebSocketSharp;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        WebSocket ws;
+        public Form1()
+        {
+            InitializeComponent();
+            ws = new WebSocket("wss://themis.arti.ee/socket");
+            ws.OnMessage += onMessage;
+            ws.Connect();
+
+        }
+
+        private void onMessage(object sender, MessageEventArgs e)
+        {
+            if (!e.IsPing)
+            {
+                textBox1.Invoke(new Action(() => textBox1.Text += e.Data));                
+                Console.WriteLine(e.Data);
+            }
+        }
+    }
+}
